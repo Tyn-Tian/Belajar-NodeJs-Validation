bc0e7954082530c9f0d897ca4a7818832bdabc09
Christian <kristian2015ok@gmail.com>
2024-06-29 13:13:03 +0700

    Add: Validation

---
+++ test/validation.test.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+
+describe("Joi", () => {
+  it("should can create schema", () => {
+    const schema = Joi.string().min(3).max(100).required();
+
+    const result = schema.validate("Tian");
+    if (result.error) {
+      console.info(result.error);
+    }
+  });
+
+  it('should can validate basic data type', () => {
+    const usernameSchema = Joi.string().email().required();
+    const isAdminSchema = Joi.boolean().required();
+    const priceSchema = Joi.number().required().min(1000).max(1000000);
+
+    const resultUsername = usernameSchema.validate("tian@gmail.com");
+    console.info(resultUsername);
+
+    const resultIsAdmin = isAdminSchema.validate("true");
+    console.info(resultIsAdmin);
+
+    const resultPrice = priceSchema.validate("10000");
+    console.info(resultPrice);
+  });
+});
