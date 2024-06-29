66479c4c7d9c792513b8cdb10eb4be4fd10c5fbd
Christian <kristian2015ok@gmail.com>
2024-06-29 13:24:30 +0700

    Add: Date Validation

---
+++ test/date.test.js
@@ -0,0 +1,10 @@
+const Joi = require("joi");
+
+describe("Joi", () => {
+  it("should can validate date", () => {
+    const birthDateSchema = Joi.date().required().max("now").min("1-1-1988");
+
+    const result = birthDateSchema.validate("12-28-2003")
+    console.log(result);
+  });
+});
