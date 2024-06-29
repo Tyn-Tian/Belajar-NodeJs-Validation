c80857518d15be6962916fa9fc349fbe7b9a248f
Christian <kristian2015ok@gmail.com>
2024-06-29 13:28:22 +0700

    Add: Validation Result

--- test/date.test.js
+++ test/date.test.js
@@ -5,6 +5,8 @@ describe("Joi", () => {
     const birthDateSchema = Joi.date().required().max("now").min("1-1-1988");
 
     const result = birthDateSchema.validate("12-28-2003")
-    console.log(result);
+    console.info(result);
+    console.info(typeof result.value);
+    console.info(typeof result.error);
   });
 });
